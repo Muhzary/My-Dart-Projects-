6f9bcc1bc0f6bda47df96ab2d4fb5ad3f97c5bb9
Muhzary <162541038+Muhzary@users.noreply.github.com>
2024-03-20 15:08:55 +0300

    Add files via upload

---
+++ Dart Functions Assignment.dart
@@ -0,0 +1,37 @@
+// Task 1
+int addTwo(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Task 2
+int subtractTwo(int num1, int num2) {
+  return num1 - num2;
+}
+
+// Task 3
+int multiplyTwo(int num1, int num2) {
+  return num1 * num2;
+}
+
+// Task 4
+double divideTwo(double num1, double num2) {
+  if (num2 != 0) {
+    return num1 / num2;
+  } else {
+    throw ArgumentError('Cannot divide by zero');
+  }
+}
+
+// Task 5
+int stringLength(String str) {
+  return str.length;
+}
+
+// Task 6
+dynamic getFirstElement(List list) {
+  if (list.isNotEmpty) {
+    return list.first;
+  } else {
+    throw ArgumentError('List is empty');
+  }
+}
